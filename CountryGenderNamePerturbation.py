beafe847d2be6e19d12b9d2d0a45242107e0f862
Valentin Barriere <valentin.barriere@ec.europa.eu>
2024-05-21 11:25:00 +0200

    Initial commit

---
+++ CountryGenderNamePerturbation.py
@@ -0,0 +1,124 @@
+"""
+Script transforming the data into perturbed data with names typical from other
+countries, in order to test the bias of the model
+Just return the examples with entities inside
+"""
+import itertools
+
+import spacy
+
+from checklist.perturb import Perturb
+
+# see: https://spacy.io/models
+nlp = spacy.load("xx_ent_wiki_sm")
+
+class PerturbedExamples:
+    def __init__(
+        self,
+        list_countries: list = [
+            "Austria",
+            "Belgium",
+            "Czech_Republic",
+            "Denmark",
+            "United_Kingdom",
+            "Estonia",
+            "Finland",
+            "France",
+            "Germany",
+            "Hungary",
+            "Iceland",
+            "Ireland",
+            "Italy",
+            "Lithuania",
+            "Malta",
+            "the_Netherlands",
+            "Norway",
+            "Poland",
+            "Portugal",
+            "Slovenia",
+            "Spain",
+            "Sweden",
+            "Switzerland",
+            "Albania",
+            "Belarus",
+            "Bosnia_and_Herzegovina",
+            "Bulgaria",
+            "Croatia",
+            "Luxembourg",
+            "Serbia",
+            "Greece",
+            "Romania",
+            "Slovakia",
+            "Ukraine",
+            "Morocco",
+            "Russia",
+            "Turkey",
+            "India",
+        ],
+        use_female: bool = True,
+    ):
+        self.list_countries = list_countries
+        self.use_female = use_female
+
+    def all_countries(self, texts, labels=None, n=2):
+        """
+        Create the new data in the form of a dictionnary
+        """
+
+        list_transformed = {country: {} for country in self.list_countries}
+        list_ex_to_tag = []
+        list_ex_to_tag_ini = []
+
+        pdata = list(nlp.pipe(texts))
+
+        # test if ok
+        if labels == None:
+            labels = [None]*len(texts)
+
+        for country, gender in itertools.product(
+            self.list_countries, ["male"] + ["female"] * self.use_female
+        ):
+            ret = Perturb.perturb(
+                pdata, Perturb.change_names_country_specific(country, gender), n=n
+            )
+            list_ex_to_tag = []
+            list_ex_to_tag_ini = []
+
+            for list_transf, lab in zip(ret.data, labels):
+                len_ex_transformed = len(list_transf[1:])
+
+                # if there are transformed data because the text contains an entity
+                if len_ex_transformed > 1:
+                    list_ex_to_tag.append((list_transf[1:], [lab] * len_ex_transformed))
+                    list_ex_to_tag_ini.append((list_transf[0], lab))
+
+            list_transformed[country][gender] = (
+                [kk for k in list_ex_to_tag for kk in k[0]],
+                [kk for k in list_ex_to_tag for kk in k[1]],
+            )
+
+            # on sauve les tweeets d'origine
+            if "Original" not in list_transformed.keys():
+                list_transformed["Original"] = (
+                    [k[0] for k in list_ex_to_tag_ini],
+                    [k[1] for k in list_ex_to_tag_ini],
+                )
+
+        return list_transformed
+
+
+if __name__ == "__main__":
+    data = [
+        "John is a very smart person, he lives in Ireland.",
+        "Luke Smith has 3 sisters.",
+        "Luke is awful",
+        "I do not like salmon",
+        "Mary came home yesterday",
+    ]
+    perturber = PerturbedExamples(list_countries=["France", "Spain", "Sweden"])
+
+    print(
+        perturber.all_countries(
+            data, ["positive", "positive", "negative", "negative", "neutral"]
+        )
+    )
---
+++ README.md
@@ -0,0 +1,110 @@
+# Biases PPL
+
+## Requirements
+
+- Python version == 3.10
+
+### CheckList library
+
+This repository uses a custom version of the [CheckList library](https://github.com/marcotcr/checklist)
+
+### Required packages
+
+For managing Python packages, it is recommended to use either `virtualenv` or `conda`.
+
+- For `virtualenv`, you can create a virtual environment using the following commands:
+
+```bash
+pip install virtualenv
+virtualenv biases-ppl
+source biases-ppl/bin/activate
+```
+
+- For `conda`, you can create a conda environment using the provided environment file:
+
+```bash
+conda env create -f environment.yml
+conda activate biases-ppl
+```
+
+The required packages are listed in the `requirements.txt` file. You can install them using `pip`.
+
+```bash
+pip install -r requirements.txt
+```
+
+### Environment file
+
+We use a `.env` file to store environment variables. Please copy the template from `public.env` to `.env` and fill in the required values.
+
+```bash
+cp public.env .env
+```
+
+### Entity Recognition Model
+
+To run the experiments we use an entity recognition model. Download it using the Spacy library:
+
+```bash
+python -m spacy download xx_ent_wiki_sm
+```
+
+### XLM-T data
+
+We use the tweets available in the repository of XLM-T. Clone the repo and generate the data in tsv format:
+
+```bash
+git clone git@github.com:cardiffnlp/xlm-t.git
+python build_xlmt_tsv_data.py
+```
+
+## Run the experiments
+
+
+### One-liners
+
+To perturb the data and calculate the bias without calculating perplexity, run:
+
+```bash
+python biases_calculation.py \
+--name_corpora tsv_data_xlmt \
+--data_tsv tweets_test_english.tsv \
+--label_type int \
+--list_countries France United_Kingdom Spain Germany Italy Morocco \
+Portugal Hungary Poland Turkey \
+--n_duplicates 50 \
+--model_name cardiffnlp/twitter-xlm-roberta-base-sentiment
+```
+
+This command will create a new file inside the input folder with "biases" as prefix. In the example, the new file will be "biases_tweets_test_english.tsv".
+
+After running the biases calculation over the tweets, you can build the confusion matrix for the data using the following command:
+
+```bash
+python build_confusion_matrix.py tsv_data_xlmt "biases_tweets_test_{}.tsv"
+```
+
+Where the first argument is the folder containing the biases calculation and the second argument is the pattern of the biases file names
+
+
+### Class
+
+You can use the class `BiasesCalculator` from `biases_calculator.py` in order to calculate the bias for a specific dataset and model, like in the `biases_calculation.py` script. 
+
+```python
+biases_calculator = BiasesCalculator(args.model_name,
+                                     path_corpus,
+                                     args.data_tsv,
+                                     args.text_col,
+                                     args.label_col,
+                                     args.list_countries,
+                                     args.n_duplicates)
+
+X_text, y = biases_calculator.read_tsv_to_inputs_data() # Reads the data and loads it as tweet, label
+
+df_bias = biases_calculator._calculate_sentiment_bias(
+    X_text,
+    y
+)
+print(df_bias)
+```
